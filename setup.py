6a558df4c584f13871904421714b03734192e774
Prasanna Raut <prasannaraut36@gmail.com>
2024-06-22 05:37:50 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup
 
 setup(
     name='analyze_stocks_india',
-    version='1.0.0',
+    version='1.0.1',
     description='A python package to download historic stock prices from NSE website and process them to find best stock to purchase',
     author='Prasanna Raut',
     author_email='prasannaraut36@gmail.com',
-    packages=['analyze_stocks_india','sample_scripts'],
+    packages=['analyze_stocks_india', 'analyze_stocks_india_sample_scripts'],
     install_requires=[
-        'numpy>=2.0.0',
-        'pandas>=2.2.2',
-        'nltk>=3.8.1',
-        'plotly>=5.22.0',
-        'requests>=2.32.3',
-        'bs4',
-        'tenacity>=8.3.0',
+        "numpy>=1.19.5",
+        "pandas>=1.1.5",
+        "nltk>=3.6.7",
+        "plotly>=5.18.0",
+        "requests>=2.27.1",
+        "bs4",
+        "tenacity>=8.2.2",
     ],
 )
